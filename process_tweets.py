e3205b7bc122b76a2b17a03483cf594e8cdf5d65
Gowtham Prudhvi Vadisetti <gowthamprudhvi@nference.net>
2021-04-03 19:23:23 +0530

    Get links

--- process_tweets.py
+++ process_tweets.py
@@ -6,6 +6,7 @@ from langdetect import detect
 from langdetect import detect_langs
 from bs4 import BeautifulSoup
 from bs4 import NavigableString
+import time
 
 json_file_list = sys.argv[1]
 
@@ -21,31 +22,36 @@ def end_node(tag):
         return False
     return True #if valid it reaches here
 
+all_links = set()
 with open(json_file_list) as fp:
     for line in fp:
         total_count = 0
         outlink_count = 0
         te_count = 0
-        # print(line)
+        print(line.strip())
         with open(line.strip()) as jsonfp:
             for json_line in jsonfp:
                 total_count += 1
-                if total_count < 3:
-                    continue
+                # print(total_count, outlink_count)
+                # if total_count < 3:
+                #     continue
                 curr_dict = json.loads(json_line.strip())
                 # print(curr_dict)
                 # quit()
                 if len(curr_dict['outlinks']) != 0:
+                    # print(len(curr_dict['outlinks']))
                     outlink_count += 1
-                    page = requests.get(curr_dict['outlinks'][0])
-                    print(page)
-                    soup = BeautifulSoup(page.content, 'html.parser')
-                    # print(soup.prettify())
-                    # print(curr_dict['outlinks'][0])
+                    all_links.update(curr_dict['outlinks'])
+                    # page = requests.get(curr_dict['outlinks'][0])
+                    # time.sleep(1)
+                    # print(page)
+                    # soup = BeautifulSoup(page.content, 'html.parser')
+                    # # print(soup.prettify())
+                    # # print(curr_dict['outlinks'][0])
                     
-                    content = soup.find_all(end_node)
-                    print(content[2]) #all end nodes matching our criteria
-                    quit()
+                    # content = soup.find_all(end_node)
+                    # print(content[3]) #all end nodes matching our criteria
+                    # quit()
                     # print(detect_langs(curr_dict['content']))
                     # quit()
                     # try:
@@ -56,5 +62,12 @@ with open(json_file_list) as fp:
                     #     # pass
                     # print(detect_langs(curr_dict['content']))
                     # print(curr_dict['content'], curr_dict['outlinks'])
-        print(total_count, outlink_count, te_count)
-        quit()
+        # print(total_count, outlink_count, te_count)
+        # quit()
+        # break
+
+
+for link in all_links:
+    print(link)
+
+# print(len(all_links))
